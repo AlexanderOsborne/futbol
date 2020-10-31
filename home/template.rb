300747b4b452273784d8031932aed327c236d73a
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-31 12:02:49 -0700

    Created all Stat pages, and Game stat Highest total Score working

--- home/template.rb
+++ home/template.rb
@@ -5,7 +5,7 @@ module Template
       <html lang="en-US">
         <head>
           <meta charset = "UTF-8">
-          <title> Futbol Stats</title>
+          <title> Futbol Stats</title> <a href="./index.html"> Home Page</a>
           <link rel="stylesheet" href="stylesheet.css" />
         </head>
 
@@ -14,11 +14,78 @@ module Template
           <hr>
 
           <h2>Site Index</h2>
-          <ul>
-            <h3>Game Stats</h3>
-            <h3>Team Stats</h3>
-            <h3><Season Stats</h3>
-          </ul>
+          <ol>
+            <h3><a href="./game_stats.html"> Game Stats </a></h3>
+            <h3><a href="./team_stats.html"> Team Stats </a></h3>
+            <h3><a href="./league_stats.html">League Stats</a></h3>
+            <h3><a href="./season_stats.html">Season Stats</a></h3>
+          </ol>
+        </body>
+      </html>
+    }
+  end
+
+  def game_stats_template
+    %{
+      <html lang="en-US">
+        <head>
+          <meta charset = "UTF-8">
+          <title> Team Statistics </title>
+          <link rel="stylesheet" href="stylesheet.css" />
+        </head>
+
+        <body>
+          <h1> Team Stats </h1>
+          <td> Highest Total Score </td>
+          <td><%= @stat_tracker.highest_total_score %><td>
+        </body>
+      </html>
+    }
+  end
+
+  def team_stats_template
+    %{
+      <html lang="en-US">
+        <head>
+          <meta charset = "UTF-8">
+          <title> Team Statistics </title>
+          <link rel="stylesheet" href="stylesheet.css" />
+        </head>
+
+        <body>
+          <h1> Team Stats </h1>
+        </body>
+      </html>
+    }
+  end
+
+  def league_stats_template
+    %{
+      <html lang="en-US">
+        <head>
+          <meta charset = "UTF-8">
+          <title> Team Statistics </title>
+          <link rel="stylesheet" href="stylesheet.css" />
+        </head>
+
+        <body>
+          <h1> Team Stats </h1>
+        </body>
+      </html>
+    }
+  end
+
+  def season_stats_template
+    %{
+      <html lang="en-US">
+        <head>
+          <meta charset = "UTF-8">
+          <title> Team Statistics </title>
+          <link rel="stylesheet" href="stylesheet.css" />
+        </head>
+
+        <body>
+          <h1> Team Stats </h1>
         </body>
       </html>
     }
--- page_generator.rb
+++ page_generator.rb
@@ -9,10 +9,30 @@ class PageGenerator
   def initialize(locations)
     @stat_tracker = StatTracker.from_csv(locations)
     @home_page_template = home_page_template
+    @game_stats_template = game_stats_template
+    @team_stats_template = team_stats_template
+    @league_stats_template = league_stats_template
+    @season_stats_template = league_stats_template
   end
 
   def render_home_page
     ERB.new(@home_page_template).result(binding)
+  end
+
+  def render_game_stats
+    ERB.new(@game_stats_template).result(binding)
+  end
+
+  def render_team_stats
+    ERB.new(@team_stats_template).result(binding)
+  end
+
+  def render_league_stats
+    ERB.new(@league_stats_template).result(binding)
+  end
+
+  def render_season_stats
+    ERB.new(@season_stats_template).result(binding)
   end
 end
 
@@ -29,3 +49,7 @@ locations = {
 page_generator = PageGenerator.new(locations)
 
 File.open('./home/index.html', 'w') {|file| file.write(page_generator.render_home_page)}
+File.open('./home/game_stats.html', 'w') {|file| file.write(page_generator.render_game_stats)}
+File.open('./home/team_stats.html', 'w') {|file| file.write(page_generator.render_team_stats)}
+File.open('./home/league_stats.html', 'w') {|file| file.write(page_generator.render_league_stats)}
+File.open('./home/season_stats.html', 'w') {|file| file.write(page_generator.render_season_stats)}
