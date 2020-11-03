7b7ccfbbc66deaf62057a7c1ec070ecac2d3b360
Liam Cusack <LCusack8108@gmail.com>
2020-11-03 12:28:53 -0700

    Add 3 working tests in SeasonStatTest.rb

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,7 +1,19 @@
+require_relative './calculator'
+
 class SeasonStats
-  attr_reader
+  extend Calculator
 
-  def initialize
+  def self.fewest_tackles(game_teams)
+    low(teams_with_tackles(game_teams)).first
+  end
 
+  def self.teams_with_tackles(game_teams)
+    game_teams.each_with_object(Hash.new(0)) do |game_team, teams|
+      teams[game_team.team_id] += game_team.tackles
+    end
+  end
+
+  def self.most_tackles(game_teams)
+    high(teams_with_tackles(game_teams)).first
   end
 end
